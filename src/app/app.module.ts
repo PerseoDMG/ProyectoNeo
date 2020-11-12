d76b4d6c7a1bb8593212956f8f850c052c87b515
Daniel Muñoz <e-dguerrero@neoris.com>
2020-11-12 13:16:04 -0600

    Archivo rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
-import {Route, RouterModule} from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { UserListComponent } from './components/user-list/user-list.component';
+import { Routes } from "./app.routes";
 
 import { DataService } from './data.service';
-import { UserNamesComponent } from './user-names/user-names.component';
-import { HomeComponent } from './home/home.component';
 
-const routes: Route[] = [
-  {path: '', component: HomeComponent},
-  {path: 'userList', component: UserListComponent},
-  {path: 'userName', component: UserNamesComponent}
-];
+
+
 
 @NgModule({
   declarations: [
@@ -30,7 +25,7 @@ const routes: Route[] = [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    Routes
   ],
   //Servicios
   providers: [DataService],
